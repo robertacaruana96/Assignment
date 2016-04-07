e5b04a55a875d918255d951ae48073220f93eee1
Leanne Marie Vassallo <leanne.vassallo.14@um.edu.mt>
2016-04-07 11:22:35 +0000

    Testing commit for some basic code in intelliJ

---
+++ Testing_IntelliJ/src/test1.java
@@ -0,0 +1,17 @@
+/**
+ * Created by leann on 07/04/2016.
+ */
+public class test1 {
+
+    public static void main (String[] args) {
+
+        int i = 10;
+
+        if (i > 100)
+            System.out.println("i is greater than 100");
+        else if (i > 50)
+            System.out.println("i is greater than 50");
+        else
+            System.out.println("i is less than 50");
+    }
+}
---
+++ Testing_IntelliJ/src/test2.java
@@ -0,0 +1,10 @@
+/**
+ * Created by leann on 07/04/2016.
+ */
+public class test2 {
+
+    public static void main (String [] args){
+
+        System.out.println("Hello My name is Leanne");
+    }
+}
