381b6290ed6bae717c79320e1891e8a69d7c72ff
Roberta Caruana <caruana.rob@gmail.com>
2016-04-20 10:06:08 +0200

    Field Already Exists Exception Class

---
+++ src/main/java/com/User/FieldAlreadyExistsException.java
@@ -0,0 +1,14 @@
+package com.User;
+
+/**
+ * Created by Bobby on 19-Apr-16.
+ */
+
+    public class FieldAlreadyExistsException extends Exception
+    {
+        public FieldAlreadyExistsException(String message)
+        {
+            super(message);
+        }
+    }
+
