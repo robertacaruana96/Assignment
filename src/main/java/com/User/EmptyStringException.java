8e47ef0a80cd05fa7296d9769db9d97fae510475
Roberta Caruana <caruana.rob@gmail.com>
2016-04-20 10:05:29 +0200

    Empty String Exception Class

---
+++ src/main/java/com/User/EmptyStringException.java
@@ -0,0 +1,13 @@
+package com.User;
+
+/**
+ * Created by Bobby on 19-Apr-16.
+ */
+    public class EmptyStringException extends Exception
+    {
+        public EmptyStringException(String message)
+        {
+            super(message);
+        }
+    }
+
