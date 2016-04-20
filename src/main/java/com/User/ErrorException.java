03eda6db961c0223879d1c5c46462277a69d2857
Roberta Caruana <caruana.rob@gmail.com>
2016-04-20 10:05:43 +0200

    Error Exception Class

---
+++ src/main/java/com/User/ErrorException.java
@@ -0,0 +1,10 @@
+package com.User;
+
+/**
+ * Created by Bobby on 19-Apr-16.
+ */
+public class ErrorException extends Exception{
+    public ErrorException(String message){
+        super(message);
+    }
+}
