7655e52599b47f434a5a40c9f6d19e509f124e39
Roberta Caruana <caruana.rob@gmail.com>
2016-04-23 07:27:49 +0200

    Arranged method to add the Date Loan

--- src/main/java/com/User/Book.java
+++ src/main/java/com/User/Book.java
@@ -3,101 +3,119 @@ package com.User;
 /**
  * Created by leann on 13/04/2016.
  */
-public class Book {
+public class Book
+{
 
     private int bookId;
     private String isbn;
     private String title;
     private String author;
-    private String genre;
     private int yearOfPublication;
     private int edition;
     private boolean isBorrowed;
-    private boolean borrowed; //flag to see if the book has been borrowed or not
+    private Genre genre;
 
-    public Book(String isbn, int bookId, String title, String author, String genre, int edition, int yearOfPublication) {
+    public Book(String isbn, int bookId, String title, String author, Genre genre, int edition, int yearOfPublication)
+    {
         this.isbn = isbn;
         this.bookId = bookId;
         this.title = title;
         this.author = author;
-        this.genre = genre;
         this.edition = edition;
         this.yearOfPublication = yearOfPublication;
+        this.genre = genre;
     }
 
 
-    public Book(){
+    public Book()
+    {
         this.isbn = null;
         this.bookId = 0;
         this.title = null;
         this.author = null;
-        this.genre = null;
         this.yearOfPublication = 0;
         this.edition = 0;
+        this.genre = null;
         this.isBorrowed = false;
     }
 
-    public int getBookId() {
+    public int getBookId()
+    {
         return bookId;
     }
 
-    public void setBookId(int bookId) {
+    public void setBookId(int bookId)
+    {
         this.bookId = bookId;
     }
 
-    public String getIsbn() {
+    public String getIsbn()
+    {
         return isbn;
     }
 
-    public void setIsbn(String isbn) {
+    public void setIsbn(String isbn)
+    {
         this.isbn = isbn;
     }
 
-    public String getTitle() {
+    public String getTitle()
+    {
         return title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title)
+    {
         this.title = title;
     }
 
-    public String getAuthor() {
+    public String getAuthor()
+    {
         return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(String author)
+    {
         this.author = author;
     }
 
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getYearOfPublication() {
+    public int getYearOfPublication()
+    {
         return yearOfPublication;
     }
 
-    public void setYearOfPublication(int yearOfPublication) {
+    public void setYearOfPublication(int yearOfPublication)
+    {
         this.yearOfPublication = yearOfPublication;
     }
 
-    public int getEdition() {
+    public int getEdition()
+    {
         return edition;
     }
 
-    public void setEdition(int edition) {
+    public void setEdition(int edition)
+    {
         this.edition = edition;
     }
 
-    public boolean isBorrowed() {
-        return borrowed;
+    public Genre getGenre()
+    {
+        return genre;
     }
 
-    public void setBorrowed(boolean borrowed) {
-        this.borrowed = borrowed;
+    public void setGenre(Genre genre)
+    {
+        this.genre = genre;
+    }
+
+    public boolean isBorrowed()
+    {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed)
+    {
+        isBorrowed = borrowed;
     }
 }
