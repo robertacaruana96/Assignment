be87ab868071a5d2d78f80777d0661e4a9826297
Roberta Caruana <caruana.rob@gmail.com>
2016-05-03 12:51:24 +0200

    Main filter factory class

--- src/main/java/com/User/FilterFactory.java
+++ src/main/java/com/User/FilterFactory.java
@@ -4,4 +4,25 @@ package com.User;
  * Created by Bobby on 03-May-16.
  */
 public class FilterFactory {
+
+    public Filter getFilter(String filterType)
+    {
+        if(filterType == null)
+        {
+            return null;
+        }
+        if(filterType == "TITLE")
+        {
+            return new searchByTitle();
+        }
+        else if(filterType == "GENRE")
+        {
+            return new SearchByGenre();
+        }
+        else if(filterType == "YEAROFPUBLICATION")
+        {
+            return new SearchByYearOfPublication();
+        }
+        return null;
+    }
 }
